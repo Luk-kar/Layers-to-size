0ca6856879ad48eaaa1e38b2106b4f1301cca3b4
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-10-17 22:17:00 +0200

    it works

--- LayersToSizes.jsx
+++ LayersToSizes.jsx
@@ -4,17 +4,17 @@ function LayersToSizes() {
     app.displayDialogs = DialogModes.NO;
 
     var doc = app.activeDocument;
-    var docPath = doc.path;
+    var docDir = doc.path;
     var filePath = doc.fullName
 
     var sizes = getConfigValue();
 
     doc.close();
 
-    for (var i = 0; i < sizes.length; i++) {
+    for (var j = 0; j < sizes.length; j++) {
         var reopenedFile = app.open(new File(filePath))
 
-        var newSize = sizes[i]
+        var newSize = sizes[j]
 
         var folderSizeName = newSize[0]
         var width = newSize[1]
@@ -23,6 +23,41 @@ function LayersToSizes() {
         var unit = "px"
 
         reopenedFile.resizeImage(UnitValue(width, unit), UnitValue(height, unit), undefined, ResampleMethod.BICUBICSHARPER)
+
+        var layers = reopenedFile.artLayers;
+
+
+        //save as in folder
+        for (k = 0; k < layers.length; k++) {
+            // make all layers invible
+            for (var i = 0; i < layers.length; i++) {
+                var layer = layers[i]
+                layer.visible = false;
+            }
+            // layer name
+            var layer = layers[k]
+
+            layer.visible = true;
+            var fileName = layer.name
+
+            //create folder
+            var newFolder = new Folder(docDir + "/" + folderSizeName)
+
+            if (!newFolder.exists) {
+                newFolder.create();
+            }
+
+            // layer visible = true
+            var newFilePath = docDir + "/" + folderSizeName +"/" + fileName + ".png"
+
+            var pngFile = File(newFilePath);
+            pngSaveOptions = new PNGSaveOptions();
+
+            // alert("stop")
+
+            reopenedFile.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
+        }
+
         reopenedFile.close()
     }
 
