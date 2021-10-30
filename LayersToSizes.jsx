3a07556c4bd629cc1032c552457a34c692f20c2b
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-10-30 19:14:50 +0200

    update

--- LayersToSizes.jsx
+++ LayersToSizes.jsx
@@ -1,8 +1,6 @@
 function LayersToSizes() {
 
-    // set app configuration to run script
-    var appOptionsToRestore = app.displayDialogs;
-    app.displayDialogs = DialogModes.NO;
+    var appOptionsToRestore = setAppConfigurationToRunScript();
 
     var doc = app.activeDocument;
     var fileData = getFileData(doc)
@@ -10,8 +8,28 @@ function LayersToSizes() {
 
     setFileToPngs(fileData);
 
-    app.open(new File(fileData.filePath))
+    restoreStartingConfiguration(fileData, appOptionsToRestore);
+    informClientAboutSuccess();
+}
+
+function informClientAboutSuccess() {
+    var doc = app.activeDocument
+    var docDir = decodeURI(doc.path)
+    alert("You have successfully saved layers to desired pngs\n" + docDir);
+}
+
+function restoreStartingConfiguration(fileData, appOptionsToRestore) {
+
+    app.open(new File(fileData.filePath));
     app.displayDialogs = appOptionsToRestore;
+}
+
+function setAppConfigurationToRunScript() {
+
+    var appOptionsToRestore = app.displayDialogs;
+    app.displayDialogs = DialogModes.NO;
+
+    return appOptionsToRestore;
 }
 
 function getFileData(doc) {
@@ -26,54 +44,64 @@ function setFileToPngs(file) {
     var sizes = getConfigValue();
 
     for (var j = 0; j < sizes.length; j++) {
-        var reopenedFile = app.open(new File(file.filePath));
 
+        var reopenedFile = app.open(new File(file.filePath));
         var newSize = sizes[j];
 
         var folderSizeName = newSize[0];
-        var width = newSize[1];
-        var height = newSize[2];
 
-        var unit = "px";
+        setNewSize(reopenedFile, newSize);
 
-        reopenedFile.resizeImage(UnitValue(width, unit), UnitValue(height, unit), undefined, ResampleMethod.BICUBICSHARPER);
-
-        var layers = reopenedFile.artLayers;
-
-
-        //save as in folder
-        for (k = 0; k < layers.length; k++) {
-            // make all layers invible
-            for (var i = 0; i < layers.length; i++) {
-                var layer = layers[i];
-                layer.visible = false;
-            }
-            // layer name
-            var layer = layers[k];
-
-            layer.visible = true;
-            var fileName = layer.name;
-
-            //create folder
-            var newFolder = new Folder(file.docDir + "/" + folderSizeName);
-
-            if (!newFolder.exists) {
-                newFolder.create();
-            } else {
-                newFolder.remove();
-            }
-
-            // layer visible = true
-            var newFilePath = file.docDir + "/" + folderSizeName + "/" + fileName + ".png";
-
-            var pngFile = File(newFilePath);
-            pngSaveOptions = new PNGSaveOptions();
-
-            reopenedFile.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
-        }
+        saveLayersToPngs(folderSizeName);
 
         reopenedFile.close(SaveOptions.DONOTSAVECHANGES);
     }
+}
+
+function saveLayersToPngs(folderSizeName) {
+
+    var doc = app.activeDocument
+    var file = getFileData(doc)
+    var layersToPngs = doc.artLayers;
+
+    for (k = 0; k < layersToPngs.length; k++) {
+        // make all layers invible
+        for (var i = 0; i < layersToPngs.length; i++) {
+            var layer = layersToPngs[i];
+            layer.visible = false;
+        }
+        // layer name
+        var layer = layersToPngs[k];
+
+        layer.visible = true;
+        var fileName = layer.name;
+
+        //create folder
+        var newFolder = new Folder(file.docDir + "/" + folderSizeName);
+
+        if (!newFolder.exists) {
+            newFolder.create();
+        } else {
+            newFolder.remove();
+        }
+
+        // layer visible = true
+        var newFilePath = file.docDir + "/" + folderSizeName + "/" + fileName + ".png";
+
+        var pngFile = File(newFilePath);
+        pngSaveOptions = new PNGSaveOptions();
+
+        doc.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
+    }
+}
+
+function setNewSize(reopenedFile, newSize) {
+
+    var width = newSize[1];
+    var height = newSize[2];
+    var unit = "px";
+
+    reopenedFile.resizeImage(UnitValue(width, unit), UnitValue(height, unit), undefined, ResampleMethod.BICUBICSHARPER);
 }
 
 function getConfigPath() {
